6f9f20e2c18b865c94997adeb8ead39e20be2241
Nathan Fredrickson <fredricksonn@spu.edu>
2021-02-06 21:36:23 -0700

    moving deck_generator to a class

--- README.md
+++ README.md
@@ -6,3 +6,31 @@ This is a package to create playable decks for Dominion, based on what Expansion
 https://medium.com/analytics-vidhya/web-scraping-a-wikipedia-table-into-a-dataframe-c52617e1f451
 ## Wiki Table of all cards
 http://wiki.dominionstrategy.com/index.php?title=List_of_cards&action=edit
+
+# ToDo list
+Create QT Gui
+- Blank Gui
+- Add user options
+- Add/animate images
+- Interact with Algorithm
+
+Create interface for Django
+Develop Weightings
+Decide on user options
+- Number of cards (10/12)
+- +2 Action
+- +2 Buy
+- +2+ Cards
+- Mean of distribution
+- Attack (y/n)
+- - Reaction (y/n)
+- Choose Distribution type
+
+# Engine
+Steps 2 and 3a/3b can be done in parallel
+
+1. Downselect Deck to chosen expansions
+2. Create cost distribution (based on input)
+3a. Generate weightings
+3b. Draw X cards from weighted deck
+4. Fill cost distribution
--- src/deck_generator.py
+++ src/deck_generator.py
@@ -13,112 +13,119 @@ import collections
 from gui.tkinter import get_selection
 
 EDITION_RE = re.compile('^(.*), (\d)E')
-
-def unique(seq):
-    """
-    Return unique list maintaining order
-    https://stackoverflow.com/a/480227
-    """
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
-
-def downselect_sets( deck, override=[]):
-    # Get all sets in deck
-    all_sets = unique(deck['Set'])
-    set_w_editions = {}
-    for dom_set in all_sets:
-        match = EDITION_RE.match(dom_set)
-        if match:
-            set_w_editions.setdefault(match.group(1),[])
-            set_w_editions[match.group(1)].append(match.group(2))
-    
-    # Remove base name from sets with editions (you dont want Base, Base1E and Base2E)
-    for dom_set in set_w_editions:
-        all_sets.remove(dom_set)
-    
-    # Make sure editons are sorted properly
-    # TODO
-    #print(all_sets)
-    
-    
-    if not override:
-        # Get selected decks from GUI
-        selected_decks = get_selection('Deck Selection',all_sets)
-    else:
-        # Use override as deck selection
-        selected_decks = override
-    
-    # Check for selected sets with additions (you need to add back in the base name)
-    for dom_set in selected_decks:
-        match = EDITION_RE.match(dom_set)
-        if match:
-            base = match.group(1)
-            if base not in selected_decks:
-                selected_decks.append(base)
-            
-    
-    # Build slice
-    slice_ = []
-    for sel in selected_decks:
-        print(sel)
-        if len(slice_)>0:
-            slice_ = slice_ | (deck['Set']==sel)
-        else:
-            slice_ = deck['Set']==sel
-    
-    # Downselect
-    if len(slice_)>0:
-        downselect = deck[slice_]
-    else:
-        downselect = deck
-    return downselect
-
 Stats = collections.namedtuple('Stats',['min','max','mean'])
 
-# Given upper and lower bounds, can calculate sigma and mean
-# returns a sorted list of random ints bewteen upper and lower bounds
-def gaussDist( upper, lower, sz=10, sigma=-1, mean=-1):
-    if mean == -1:
-        mean = (upper + lower)/2
-        print("Using calculated mean : {}".format(str(mean)))
-    if sigma == -1:
-        sigma = (mean-lower)/2
-        print("Using calculated sigma: {}".format(str(sigma)))
+class DeckGenerator():
 
-    results = random.default_rng().normal(loc=mean,scale=sigma,size=sz)
-    results = [ math.floor(x) for x in results ]
-    results = [ lower if x < lower else x for x in results ]
-    results = [ upper if x > upper else x for x in results ]
-    results.sort()
-    return results
+    def unique(self, seq):
+        """
+        Return unique list maintaining order
+        https://stackoverflow.com/a/480227
+        """
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
 
-def getStats (df, field='Cost'):
-    S = Stats(min=df[field].min(),max=df[field].max(),mean=-1)
-    return S
+    def downselect_sets(self, deck, override=[]):
+        # Get all sets in deck
+        all_sets = self.unique(deck['Set'])
+        set_w_editions = {}
+        for dom_set in all_sets:
+            match = EDITION_RE.match(dom_set)
+            if match:
+                set_w_editions.setdefault(match.group(1),[])
+                set_w_editions[match.group(1)].append(match.group(2))
+        
+        # Remove base name from sets with editions (you dont want Base, Base1E and Base2E)
+        for dom_set in set_w_editions:
+            all_sets.remove(dom_set)
+        
+        # Make sure editons are sorted properly
+        # TODO
+        #print(all_sets)
+        
+        
+        if not override:
+            # Get selected decks from GUI
+            selected_decks = get_selection('Deck Selection',all_sets)
+        else:
+            # Use override as deck selection
+            selected_decks = override
+        
+        # Check for selected sets with additions (you need to add back in the base name)
+        for dom_set in selected_decks:
+            match = EDITION_RE.match(dom_set)
+            if match:
+                base = match.group(1)
+                if base not in selected_decks:
+                    selected_decks.append(base)
+                
+        
+        # Build slice
+        slice_ = []
+        for sel in selected_decks:
+            print(sel)
+            if len(slice_)>0:
+                slice_ = slice_ | (deck['Set']==sel)
+            else:
+                slice_ = deck['Set']==sel
+        
+        # Downselect
+        if len(slice_)>0:
+            downselect = deck[slice_]
+        else:
+            downselect = deck
+        return downselect
 
-def getCostDistOfKingdomCards(df):
-    kingdomCards = df[df['CardClass'] != 'Basic']
-    stats = getStats(kingdomCards,'CostValue')
-    return gaussDist(int(stats.max), int(stats.min))
 
-# lambda function to get the value of cost and create a new col for it
-def getValFromCost(row):
-    cost=row['Cost']
-    if type(cost) == int:
-        return cost
-    if '$' in cost:
-        return cost.replace('$','')
+
+    # Given upper and lower bounds, can calculate sigma and mean
+    # returns a sorted list of random ints bewteen upper and lower bounds
+    def gaussDist(self, upper, lower, sz=10, sigma=-1, mean=-1):
+        if mean == -1:
+            mean = (upper + lower)/2
+            print("Using calculated mean : {}".format(str(mean)))
+        if sigma == -1:
+            sigma = (mean-lower)/2
+            print("Using calculated sigma: {}".format(str(sigma)))
+
+        results = random.default_rng().normal(loc=mean,scale=sigma,size=sz)
+        results = [ math.floor(x) for x in results ]
+        results = [ lower if x < lower else x for x in results ]
+        results = [ upper if x > upper else x for x in results ]
+        results.sort()
+        return results
+
+    def getStats (self, df, field='Cost'):
+        S = Stats(min=df[field].min(),max=df[field].max(),mean=-1)
+        return S
+
+    def getCostDistOfKingdomCards(self, df):
+        kingdomCards = df[df['CardClass'] != 'Basic']
+        stats = self.getStats(kingdomCards,'CostValue')
+        return self.gaussDist(int(stats.max), int(stats.min))
+
+    # lambda function to get the value of cost and create a new col for it
+    def getValFromCost(self, row):
+        cost=row['Cost']
+        if type(cost) == int:
+            return cost
+        if '$' in cost:
+            return cost.replace('$','')
+
+    def runMain(self):
+        deck = pd.read_csv('data/dominion_cards.csv')
+        override = ['Base, 2E', 'Intrigue, 2E']#'Base, 2E', 'Intrigue, 2E', 'Seaside', 'Prosperity']
+        deck = self.downselect_sets( deck, override=override )
+
+        # add CostValue column
+        deck['CostValue'] = deck.apply(lambda row: self.getValFromCost(row),axis=1)
+
+        Dist = self.getCostDistOfKingdomCards(deck)
+        print(Dist)    
+        # deck, supply = separate_supply( desk )
+        deck.to_csv('temp.csv')    
 
 if __name__=='__main__':
-    deck = pd.read_csv('data/dominion_cards.csv')
-    override = ['Base, 2E', 'Intrigue, 2E']#'Base, 2E', 'Intrigue, 2E', 'Seaside', 'Prosperity']
-    deck = downselect_sets( deck, override=override )
-
-    # add CostValue column
-    deck['CostValue'] = deck.apply(lambda row: getValFromCost(row),axis=1)
-
-    Dist = getCostDistOfKingdomCards(deck)
-    print(Dist)    
-    # deck, supply = separate_supply( desk )
-    deck.to_csv('temp.csv')
+    DG = DeckGenerator()
+    DG.runMain()
